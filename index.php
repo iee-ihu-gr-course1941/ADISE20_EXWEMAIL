12b48946a75a3164bba1d6751dbaee4703cbcace
jim3692 <jim3692@gmail.com>
2020-12-05 13:01:45 +0200

    Add configuration files

---
+++ index.php
@@ -0,0 +1,14 @@
+<?php $config = require('./.config.php'); ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <base href="<?php echo $config['host'] . $config['base']; ?>">
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>DominoZ</title>
+</head>
+<body>
+
+</body>
+</html>
