861f9535ef5b57b51a677960b0af4ec5137c1b4e
jim3692 <jim3692@gmail.com>
2020-12-19 00:56:51 +0000

    Implement game list endpoint

---
+++ enums.php
@@ -0,0 +1,12 @@
+<?php
+
+define('GAME_STATUS_WAITING_PLAYERS', 'waitingForPlayers');
+define('GAME_STATUS_RUNNING', 'running');
+define('GAME_STATUS_ENDED', 'ended');
+
+define('GSTATE_FIELD_HOST', 'host');
+define('GSTATE_FIELD_CURRENT_PLAYER', 'currentPlayer');
+define('GSTATE_FIELD_BOARD', 'board');
+
+define('PSTATE_FIELD_READY', 'ready');
+define('PSTATE_FIELD_HAND', 'hand');
--- model/game.php
+++ model/game.php
@@ -1,13 +1,19 @@
 <?php namespace model;
 
-class Game
+class Game implements \JsonSerializable
 {
     private $db;
-    private $id;
     private $user;
+
+    private $id;
+    private $code;
+    private $players;
+    private $state;
 
     public function __construct()
     {
+        include dirname(__FILE__) . '/../enums.php';
+
         $this->db = require(dirname(__FILE__) . '/../database/db.php');
         if (!isset($_SESSION['user'])) {
             die('Not logged in');
@@ -33,8 +39,8 @@ class Game
             die('Insertion failed: ' . $this->db->error);
         }
 
-        $code = $this->generateRandomString();
-        $stmt->bind_param('si', $code, $status);
+        $this->code = $this->generateRandomString();
+        $stmt->bind_param('si', $this->code, $status);
         if (!$stmt->execute()) {
             die('Insertion failed: ' . $stmt->error);
         }
@@ -88,6 +94,145 @@ class Game
 
     public function leave()
     {
+    }
+
+    public function getList()
+    {
+        $sql = "SELECT
+                games.id,
+                games.code,
+                gstate_fields.name AS gstate_field,
+                game_state.value AS gstate_value,
+                players.id as playerId,
+                users.id AS userId,
+                users.username,
+                pstate_fields.name AS pstate_field,
+                player_state.value AS pstate_value
+            FROM games
+            JOIN game_state
+                ON game_state.game = games.id
+            JOIN enums AS gstate_fields
+                ON gstate_fields.id = game_state.field
+            JOIN players
+                ON players.game = games.id
+            JOIN users
+                ON users.id = players.user
+            LEFT JOIN player_state
+                ON player_state.player = players.id
+            LEFT JOIN enums AS pstate_fields
+                ON pstate_fields.id = player_state.field
+            WHERE games.status = (
+                SELECT id
+                FROM enums
+                WHERE name = 'GAME_STATUS_WAITING_PLAYERS'
+            )
+        ";
+        $stmt = $this->db->prepare($sql);
+        if (!$stmt) {
+            die('List failed: ' . $this->db->error);
+        }
+
+        $stmt->execute();
+        $result = $stmt->get_result();
+        $games = $result->fetch_all(MYSQLI_ASSOC);
+        $stmt->close();
+
+        $gamesGrouped = [];
+        foreach ($games as $game) {
+            $gamesGrouped[$game['id']][] = $game;
+        }
+
+        $gamesParsed = [];
+        foreach ($gamesGrouped as $group) {
+            $game = new Game();
+            $game->fromGameGroup($group);
+            $gamesParsed[] = $game;
+        }
+
+        return $gamesParsed;
+    }
+
+    public function fromGameGroup($group)
+    {
+        if (count($group) === 0) {
+            return;
+        }
+
+        $this->id = $group[0]['id'];
+        $this->code = $group[0]['code'];
+        $this->players = [];
+
+        $this->state = [];
+        foreach ($group as $gameRow) {
+            $field = constant($gameRow['gstate_field']);
+            $this->state[$field] = $gameRow['gstate_value'];
+        }
+
+        $this->loadPlayers($group);
+
+        $this->state['host'] = (int)($this->state['host']);
+    }
+
+    public function loadPlayers($group)
+    {
+        if (count($group) === 0) {
+            return;
+        }
+
+        $this->players = [];
+
+        $playersGroupedByUser = [];
+        foreach ($group as $player) {
+            $playersGroupedByUser[$player['user']][] = $player;
+        }
+
+        foreach ($playersGroupedByUser as $playerGroup) {
+            // TODO: Implement in new model Class, Player
+            if (count($playerGroup) === 0) {
+                continue;
+            }
+
+            $player = [];
+            $player['id'] = $playerGroup[0]['playerId'];
+            $player['game'] = $playerGroup[0]['id'];
+            $player['userId'] = $playerGroup[0]['userId'];
+            $player['username'] = $playerGroup[0]['username'];
+
+            $player['state'] = [];
+            foreach ($group as $playerRow) {
+                if ($playerRow['pstate_field'] == null) {
+                    continue;
+                }
+                $field = constant($playerRow['pstate_field']);
+                $player['state'][$field] = $playerRow['pstate_value'];
+            }
+
+            $this->players[] = $player;
+        }
+    }
+
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    public function toArray()
+    {
+        if (!isset($this->id)) {
+            die('Game is not initialized');
+        }
+
+        return [
+            'id' => $this->id,
+            'code' => $this->code,
+            'players' => $this->players,
+            'state' => $this->state
+        ];
+    }
+
+    public function jsonSerialize()
+    {
+        return $this->toArray();
     }
 
     private function generateRandomString($length = 10)
