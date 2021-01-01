64a3351dd24e97872641ac9e2b18f30c91836303
kalaitzisxristos <kalaitzisxristos@hotmail.com>
2021-01-01 21:59:23 +0000

    When the user clicks a list item, call game join

--- assets/pages/lobby.js
+++ assets/pages/lobby.js
@@ -6,6 +6,21 @@
       data.forEach(function (game) {
         const entry = document.createElement('li')
         entry.innerText = game.code
+        entry.addEventListener('click', click, false)
+        entry.addEventListener('mouseover', mouseOver, false)
+        function mouseOver () {
+
+        }
+        function click () {
+          const join = new URLSearchParams()
+          join.append('game-id', game.id)
+          fetch('actions/game/join.php', {
+            method: 'POST',
+            body: join
+          })
+            .then(response => response.json())
+            .then(data => console.log(data))
+        }
         const listofgames = document.getElementById('listofgames')
         listofgames.appendChild(entry)
       })
