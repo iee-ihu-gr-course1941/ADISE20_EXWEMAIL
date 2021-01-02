f678a1ba144fbb4e0e8deefda8fce044004ef560
jim3692 <31220180+jim3692@users.noreply.github.com>
2021-01-02 20:17:06 +0200

    Optionally set game seats (#34)

--- actions/game/create.php
+++ actions/game/create.php
@@ -5,8 +5,13 @@ use model\Game;
 return (function () {
     require_once(dirname(__FILE__) . '/../../includes.php');
 
+    $seats = 2;
+    if (isset($_POST['seats'])) {
+        $seats = (int)$_POST['seats'];
+    }
+
     $game = new Game();
-    $id = $game->create();
+    $id = $game->create($seats);
 
     header('Content-Type: application/json');
     echo $id;
