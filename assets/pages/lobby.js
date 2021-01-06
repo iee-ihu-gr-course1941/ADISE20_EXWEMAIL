0705dc9dc74a964680561f8abfcde1bfd4596c55
jim3692 <jim3692@gmail.com>
2021-01-06 20:37:38 +0000

    Dynamic lobby

--- assets/pages/lobby.js
+++ assets/pages/lobby.js
@@ -2,10 +2,24 @@
 /* eslint-disable-next-line no-unused-expressions */
 ({ self, globals: { renderPage } }) => {
   const listOfGames = self.querySelector('div.ul')
+  const app = document.getElementById('app')
 
-  fetch('actions/game/list.php')
+  const interval = setInterval(() => {
+    if (app.getAttribute('data-base-current-page') !== 'lobby') {
+      clearInterval(interval)
+      return
+    }
+
+    loadGames()
+  }, 2000)
+
+  const loadGames = () => fetch('actions/game/list.php')
     .then(response => response.json())
     .then(data => {
+      while (listOfGames.firstChild) {
+        listOfGames.firstChild.remove()
+      }
+
       if (data.length === 0) {
         const entry = document.createElement('div')
         entry.className = 'li tooltip'
