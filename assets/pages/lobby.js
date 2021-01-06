c1d8d1bcc45172bd6438560f7ae1399233c5673d
kalaitzisxristos <kalaitzisxristos@hotmail.com>
2021-01-06 10:41:38 +0000

    Hovering games show [ players - if they are ready]

--- assets/pages/lobby.js
+++ assets/pages/lobby.js
@@ -6,25 +6,48 @@
   fetch('actions/game/list.php')
     .then(response => response.json())
     .then(data =>
+
       data.forEach(function (game) {
         const entry = document.createElement('div')
-        entry.className = 'li'
+        entry.className = 'li tooltip'
+
+        const extraDiv = document.createElement('div')
+        entry.appendChild(extraDiv)
 
         const codeEl = document.createElement('span')
         codeEl.innerText = game.code
-        entry.appendChild(codeEl)
+        extraDiv.appendChild(codeEl)
 
         const playersEl = document.createElement('span')
         playersEl.innerHTML = `${game.players.length}/${game.state.seats}`
         playersEl.style.float = 'right'
-        entry.appendChild(playersEl)
+        extraDiv.appendChild(playersEl)
 
-        entry.addEventListener('click', click, false)
-        entry.addEventListener('mouseover', mouseOver, false)
+        // join a game event listener
+        entry.addEventListener('click', join, false)
+
+        // mouse over shows players inside the game hovered and if they are ready
+        extraDiv.addEventListener('mouseover', mouseOver, false)
         function mouseOver () {
+          const player = game.players
+          const floatSpan = document.createElement('span')
+          const floatDiv = document.createElement('div')
+          player.forEach(function (item, index, array) {
+            const state = item.state
 
+            if (state.ready === true) {
+              floatSpan.innerHTML += item.username + ' - Ready <br/>'
+            } else {
+              floatSpan.innerHTML += item.username + ' - Not ready <br/>'
+            }
+            floatSpan.className = 'tooltiptext'
+            entry.appendChild(floatDiv)
+          })
+          floatDiv.appendChild(floatSpan)
         }
-        function click () {
+
+        // the function of joining a game
+        function join () {
           const join = new URLSearchParams()
           join.append('game-id', game.id)
           fetch('actions/game/join.php', {
@@ -41,7 +64,7 @@
   // typing animation
   /* eslint-disable-next-line no-new */
   new Typed('.typing', {
-    strings: ['Set down doubles early.', 'Set down your heavier tiles early.', 'Hold on to a variety of suits.', 'Note your opponents weak suits.', 'Work out your opponent\'s hand.','Be Aware of the Board Count', 'Don’t be Afraid to Lower the Board Count', 'Use Blank Tiles to Your Advantage'],
+    strings: ['Set down doubles early.', 'Set down your heavier tiles early.', 'Hold on to a variety of suits.', 'Note your opponents weak suits.', 'Work out your opponent\'s hand.', 'Be Aware of the Board Count', 'Don’t be Afraid to Lower the Board Count', 'Use Blank Tiles to Your Advantage'],
     typeSpeed: 60,
     backSpeed: 70,
     loop: true,
